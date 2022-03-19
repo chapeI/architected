423d2b641459ae464686a836699f9fb326a25c21
chapel <anoopanthony@gmail.com>
2022-03-19 11:47:16 -0400

    minor fixes to storage_repo

--- lib/repos/storage_repo.dart
+++ lib/repos/storage_repo.dart
@@ -6,16 +6,15 @@ import 'package:architectured/user_model.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class StorageRepo {
-  FirebaseStorage _storage = FirebaseStorage.instance;
-  AuthRepo _authRepo = locator.get<AuthRepo>();
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final AuthRepo _authRepo = locator.get<AuthRepo>();
 
   Future<String> uploadFile(File file) async {
     UserModel user = await _authRepo.getUser();
     var userUid = user.uid;
 
-    Reference storageRef = _storage.ref().child('user/profile/$userUid');
+    Reference storageRef = _storage.ref().child('user/profile/$userUid.jpg');
     UploadTask uploadTask = storageRef.putFile(file);
-    // md code had uploadTask.oncomplete, but this should work too
     var url = await uploadTask.snapshot.ref.getDownloadURL();
     return url;
   }
