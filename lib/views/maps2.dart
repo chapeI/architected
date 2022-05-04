67753bd6feb622ed4483bd355420567472527f84
aanthony <anoop.anthony@gmail.com>
2022-05-04 15:40:44 -0400

    moving to my position

--- lib/views/maps2.dart
+++ lib/views/maps2.dart
@@ -1,12 +1,12 @@
 import 'package:architectured/models/event_model.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 class Maps2 extends StatefulWidget {
   const Maps2({Key? key}) : super(key: key);
-
   @override
   State<Maps2> createState() => _Maps2State();
 }
@@ -16,23 +16,27 @@ class _Maps2State extends State<Maps2> {
   @override
   Widget build(BuildContext context) {
     var event = Provider.of<EventModel>(context);
-
     return Scaffold(
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: LatLng(30, 30)),
+        initialCameraPosition: CameraPosition(
+            target: LatLng(43.6532, -79.3832), zoom: 12), // toronto
         onMapCreated: (GoogleMapController controller) async {
           googleMapController = controller;
-          Position position = await Geolocator.getCurrentPosition();
-          print(position);
-          googleMapController
-              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-                  target: LatLng(
-                    position.latitude,
-                    position.longitude,
-                  ),
-                  zoom: 15)));
+          _moveToMyPosition();
         },
       ),
     );
+  }
+
+  _moveToMyPosition() async {
+    Position position = await Geolocator.getCurrentPosition();
+    print(position);
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(
+              position.latitude,
+              position.longitude,
+            ),
+            zoom: 15)));
   }
 }
