6a684a4f7df4013b61e43acd8b845c4c4d52ada9
chapel <anoopanthony@gmail.com>
2022-05-14 15:49:43 -0400

    push notifications learning BUT BROKEN

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:architectured/bloc/application_bloc.dart';
 import 'package:architectured/experimental/notification_practice.dart';
 import 'package:architectured/experimental/sand_box.dart';
+import 'package:architectured/models/push_notifications_model.dart';
 import 'package:architectured/services/auth_service.dart';
 import 'package:architectured/services/singletons.dart';
 import 'package:architectured/views/friends.dart';
@@ -12,6 +13,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
@@ -44,7 +46,7 @@ class MyApp extends StatelessWidget {
           value: Auth().stream,
           initialData: null,
           child: MaterialApp(
-            home: Test(),
+            home: Notifications2(),
           )),
     );
   }
@@ -69,7 +71,6 @@ class _TestState extends State<Test> {
 
   void getToken() async {
     await FirebaseMessaging.instance.getToken().then((token) {
-      print('am i alive ');
       print(token);
     });
   }
@@ -128,5 +129,88 @@ class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OverlaySupport(
+        child: MaterialApp(
+      home: Notifications2(),
+    ));
+  }
+}
+
+class Notifications2 extends StatefulWidget {
+  const Notifications2({Key? key}) : super(key: key);
+
+  @override
+  State<Notifications2> createState() => _Notifications2State();
+}
+
+class _Notifications2State extends State<Notifications2> {
+  late final FirebaseMessaging _messaging;
+  late int _totalNotificationCounter;
+  PushNotificationsModel? _notification;
+
+  void registerNotification() async {
+    _messaging = FirebaseMessaging.instance;
+
+    // notification states (not determined (null), granted(true), decline(false))
+
+    NotificationSettings settings = await _messaging.requestPermission(
+        alert: true, badge: true, provisional: false, sound: true);
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('user granted permission');
+
+      // send message
+      FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+        PushNotificationsModel notification = PushNotificationsModel(
+          title: remoteMessage.notification!.title,
+          body: remoteMessage.notification!.body,
+          dataTitle: remoteMessage.data['title'],
+          dataBody: remoteMessage.data['body'],
+        );
+
+        setState(() {
+          _totalNotificationCounter++;
+          _notification = notification;
+        });
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('push notification test')),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          'flutter notifications',
+          textAlign: TextAlign.center,
+        ),
+        NotificationBadge(totalNotifications: _totalNotificationCounter)
+      ]),
+    );
+  }
+}
+
+class NotificationBadge extends StatelessWidget {
+  final int totalNotifications;
+  const NotificationBadge({Key? key, required this.totalNotifications})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+      child: Center(
+        child: Text(totalNotifications.toString()),
+      ),
+    );
   }
 }
---
+++ lib/models/push_notifications_model.dart
@@ -0,0 +1,9 @@
+class PushNotificationsModel {
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
+
+  PushNotificationsModel(
+      {this.title, this.body, this.dataTitle, this.dataBody});
+}
